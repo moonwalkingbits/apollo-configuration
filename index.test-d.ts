cc0fd946252905f2d482fead231c26eb8877b944
Martin Pettersson <martin_pettersson@outlook.com>
2020-11-28 14:09:02 +0100

    Add copyright header to typescript test

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020 Martin Pettersson
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 import { expectAssignable, expectError, expectType } from "tsd";
 
 import {
